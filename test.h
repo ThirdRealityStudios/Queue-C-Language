00b05f5362d775ab635566b1263bf356053b648e
ThirdRealityStudios <48909311+ThirdRealityStudios@users.noreply.github.com>
2023-01-11 22:58:40 +0100

    Create test.h

---
+++ test.h
@@ -0,0 +1,6 @@
+extern void printTest(char *functionName, int testOK);
+extern int testEmpty();
+extern int testCreate();
+extern int testEnqueue();
+extern int testDequeue();
+extern int testPrintQueue();
